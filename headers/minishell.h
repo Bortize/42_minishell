c58990e301ac8ede4f3bd324568157f990e3a31b
Borja Gomez-rey delgado <bgomez-r@c1r6s3.42madrid.com>
2021-07-31 19:52:21 +0200

    more things

--- headers/minishell.h
+++ headers/minishell.h
@@ -1,8 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bgomez-r <bgomez-r@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/31 19:45:27 by bgomez-r          #+#    #+#             */
+/*   Updated: 2021/07/31 19:46:48 by bgomez-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef
-# define MINISHELL_H
+# define	MINISHELL_H
 
+typedef enum e_behavior
+{
+	default,
+	redir_in,
+	redir_out,
+	redir_in_append,
+	here_doc,
+	dollar_var,
+	pipe
+}			t_behavior;
 
-
+typedef struct s_token
+{
+	struct s_token	*next;
+	char			*text;
+	t_behavior		behavior;
+}			t_token;
 
 #endif
