1317bf88667ee4f43d6d8bea75b6b32ff93d25e2
Victor Martinez estrada <vicmarti@c1r1s6.42madrid.com>
2021-09-24 20:01:55 +0200

    Trim to clean spaces. We need to rethink parser+tokenizer

--- srcs/string_validator.c
+++ srcs/string_validator.c
@@ -6,7 +6,7 @@
 /*   By: bgomez-r <bgomez-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/20 18:43:25 by bgomez-r          #+#    #+#             */
-/*   Updated: 2021/09/24 14:19:37 by bgomez-r         ###   ########.fr       */
+/*   Updated: 2021/09/24 17:59:51 by vicmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,4 @@ int string_validator(char *line)
 	return (1);
 ///	string_validator_spaces();
 //	string_validator_etc();
-} 
+}
