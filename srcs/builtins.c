ccd1f875ca03ed7eee501c6708f4fd8714999819
Bortize <bgomez-r@c2r2s2.42madrid.com>
2021-10-22 19:22:50 +0200

    Add the echo_builtins finished

--- srcs/builtins.c
+++ srcs/builtins.c
@@ -1,29 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtins.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bgomez-r <bgomez-r@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/22 17:45:19 by bgomez-r          #+#    #+#             */
+/*   Updated: 2021/10/22 19:14:26 by bgomez-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 void	builtint_echo(char **argv, char **env);
 
 int	main(void)
 {
-	char *argv[3];
+	char *argv[5];
 	char *env[1];
 
 	env[0] = NULL;
 	argv[0] = "echo";
-	argv[1] = "hola";
-	argv[2] = NULL;
+	argv[1] = NULL;
+	argv[2] = "hello";
+	argv[3] = "texto";
+	argv[4] = NULL;
 	builtint_echo(argv, env);
 }
 
 void	builtint_echo(char **argv, char **env)
 {
 	int i;
+	int flag_n;
 
 	i = 1;
-	while (argv[i] != NULL)
+	if (argv[i] == NULL)
+	{
+		printf("\n");
+		return;
+	}
+	if ((strcmp(argv[1], "-n") == 0))
+	{
+		flag_n++;
+		i++;
+	}
+	while (argv[i + 1] != NULL)
 	{
 		printf("%s ", argv[i]);
 		i++;
 	}
-	printf("\n");
+	printf("%s", argv[i]);
+	if (flag_n != 1)
+		printf("\n");
 }
