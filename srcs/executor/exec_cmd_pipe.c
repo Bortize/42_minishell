b1fa193157467f8a8d39077e2827fa89aa39ca85
Víctor Martínez Estrada <victormartinezestrada@gmail.com>
2021-10-23 06:36:36 +0200

    Properly close pipes to avoid blocking. Child processes kept the pipe vector open, so some programs got blocked.

--- srcs/executor/exec_cmd_pipe.c
+++ srcs/executor/exec_cmd_pipe.c
@@ -6,7 +6,7 @@
 /*   By: vicmarti <vicmarti@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/18 14:35:02 by vicmarti          #+#    #+#             */
-/*   Updated: 2021/10/22 23:01:10 by vicmarti         ###   ########.fr       */
+/*   Updated: 2021/10/23 06:44:34 by vicmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,19 +19,47 @@
 
 #include <stdio.h> //TODO just placeholder printf
 /*
+static int	get_oflag()
+{//HOW TO HANDLE HERE-DOC? Write on a tmpfile, somehow have a program cat-int out into stdin that ofc, got redirected?
+	if (redir_out)
+		O_WRONLY | O_CREAT | O_TRUNC;
+	else if (redir_out_append)
+		O_WRONLY | O_CREAT | O_APPEND;
+	else if (redir_in)
+		O_RDONLY;
+}
+
 static int	open_files(t_list *redir_lst)
 {
 	int	fd;
+	int	o_flag;
 
-	fd = 0;
+	o_flag =
+	fd = open();
+	redir_lst = redir_lst->next;
 	while (redir_lst && fd != -1)
 	{
+		close(fd);
 		fd = open();
 		redir_lst = redir_lst->next;
 	}
 	return (fd);
+}*/
+
+void	clean_pipes(int (*pipev)[2], int pos, int size)
+{
+	int	i;
+
+	i = 0;
+	while (i < size)
+	{
+		if (i != pos)
+			close(pipev[i][WRITE_END]);
+		else if (i != pos - 1)
+			close(pipev[i][READ_END]);
+		i++;
+	}
 }
-*/
 
 int	exec_cmd_pipe(t_list *cmd_lst, size_t cmdn)
 {
@@ -84,7 +112,18 @@ int	exec_cmd_pipe(t_list *cmd_lst, size_t cmdn)
 				if (i != 0)
 					dup2(pipev[i - 1][READ_END], 0);
 			}
+			clean_pipes(pipev, i, cmdn);
 			//TODO redirections plx
+			/*
+			if (cmd_lst->lst_redir_out)
+			{
+				close(0);
+				redir_fd = open_files();
+				if (redir_fd == -1)
+					exit(-1); //TODO error some file couldn't be opened
+				dup2(redir_fd, 0);
+			}
+			*/
 			execve(cmd->argv[0], cmd->argv, NULL);
 			//TODO Something went wrong here
 			//Do whatever bash does here
@@ -98,20 +137,16 @@ int	exec_cmd_pipe(t_list *cmd_lst, size_t cmdn)
 		}
 	}
 	//Wait all
+	clean_pipes(pipev, -1, cmdn);
+		//TODO Do we need to do extra work here?
+		//TODO How do we get the exit status of the last pid
 	i = 0;
 	while (i < cmdn)
 	{
-		//TODO Do we need to do extra work here?
-		//TODO How do we get the exit status of the last pid
-		if (i != cmdn - 1) //Close pipes, otherwise any read'll hang
-			//Closing the pipe will 'send' EOF to any reader.
-		{
-			close(pipev[i][0]);
-			close(pipev[i][1]);
-		}
 		waitpid(pidv[i], NULL, 0);
 		i++;
 	}
+
 	free(pipev);
 	free(pidv);
 	return (0);
