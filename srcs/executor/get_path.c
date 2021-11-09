8e7109743cc7d2ce2f811ea8e16a28f10d6dab0d
Víctor Martínez Estrada <victormartinezestrada@gmail.com>
2021-11-09 22:35:14 +0100

    Get-Path function modified to be norm-friendly.

--- srcs/executor/get_path.c
+++ srcs/executor/get_path.c
@@ -6,7 +6,7 @@
 /*   By: vicmarti <vicmarti@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/30 18:29:05 by vicmarti          #+#    #+#             */
-/*   Updated: 2021/10/31 00:01:14 by vicmarti         ###   ########.fr       */
+/*   Updated: 2021/11/09 22:34:44 by vicmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,12 @@
 
 #include <unistd.h>
 
-static int	check_path(char *path)
+//File exists and -is- a regular file. False if NULL.
+static int	valid_file(char *path)
 {
 	struct stat	info;
 
-	if (!path || stat(path, &info) == -1)
-		return (-1);
-	if ((info.st_mode & S_IFREG) == 0)
-		return (-1);
-	return (0);
+	return  (!(stat(path, &info) == -1 || (info.st_mode & S_IFREG) == 0));
 }
 
 //TODO I'm adding this to the libft.
@@ -41,37 +38,70 @@ static void	ft_free_arr(void **ptr_arr)
 	free(ptr_arr);
 }
 
-//TODO Maybe check before anything else? for paths like ./a.out or ../a.out
+//Absolute paths uses '.' as current dir and '..' as the parent dir.
+//Syscalls have no issue accessing any absolute path.
+static int	is_absolute(char *path)
+{
+	return (!path || ft_strncmp(path, "./", 2) == 0
+			|| ft_strncmp(path, "../", 3) == 0);
+}
+
+//Memory management done in the side corner, hidden from the public eye.
+//Return is just error-checking.
+static void	*set_memory(char ***pathv, char *path_env)
+{
+	if (!path_env)
+		return (NULL);
+	*pathv = ft_split(path_env, ':');
+	if (!*pathv)
+	{
+		perror("MINISHELL:");
+		return (NULL);
+	}
+	return (pathv);
+}
+
+//Build a path, usually it needs a slash '/' between elements.
+//TODO add to libft?
+static char	*path_add(char *prefix, char *suffix)
+{
+	const size_t	prefix_len = ft_strlen(prefix);
+	const size_t	suffix_len = ft_strlen(suffix);
+	char			*path;
+
+	path = malloc(prefix_len + suffix_len + 2);
+	if (!path)
+		return (path);
+	ft_strcpy(path, prefix);
+	path[prefix_len] = '/';
+	ft_strcpy(path + prefix_len + 1, suffix);
+	path[prefix_len + suffix_len + 1] = '\0';
+	return (path);
+}
+
+//Find the file using the PATH environment as prefix (like sh).
 char	*get_path(char *file, char *path_env)
 {
 	char	**pathv;
 	char	**path_iter;
 	char	*full_path;
-	char	*slash_file;
 
-	//Memory management should be done better.
-	slash_file = ft_strjoin("/", file);
-	if (!slash_file)
+	if (!file || is_absolute(file))
+		return (file);
+	if (!set_memory(&pathv, path_env))
 		return (NULL);
-	pathv = ft_split(path_env, ':');
 	path_iter = pathv;
-	if (!pathv)
-	{
-		free(slash_file);
-		return (NULL);
-	}
 	full_path = NULL;
-	while (!full_path && *path_iter)
+	while (*path_iter)
 	{
-		full_path = ft_strjoin(*path_iter, slash_file);
-		write(2, full_path, ft_strlen(full_path));
-		if (check_path(full_path) != 0)
-		{
-			free(full_path);
-			full_path = NULL;
-		}
+		full_path = path_add(*path_iter, file);
+		if (valid_file(full_path))
+			break ;
+		free(full_path);
 		path_iter++;
 	}
 	ft_free_arr((void **)pathv);
-	return (full_path);
+	if (*path_iter)
+		return (full_path);
+	return (file);
 }
--- srcs/executor/start_execution.c
+++ srcs/executor/start_execution.c
@@ -6,7 +6,7 @@
 /*   By: vicmarti <vicmarti@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/07 21:45:51 by vicmarti          #+#    #+#             */
-/*   Updated: 2021/11/08 15:51:25 by vicmarti         ###   ########.fr       */
+/*   Updated: 2021/11/09 21:38:08 by vicmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ void	start_execution(t_list *cmd_lst)
 	//TODO cool idea, but what if it fails? Malloc error or something
 	//Actually, it'd just mess with execve behaviour no?
 	//ft_lstiter(cmd_lst, build_path);
-	//ft_lstiter(cmd_lst, build_argv);
+	//ft_lstiter(cmd_lst, build_argv); //TODO Again, not here
 	if (cmd_num == 1 && is_builtin())
 		run_builtin();
 	else
