9dbfb21b004d049df19c1206bc1bebea7a5656e8
Borja Gómez-Rey Delgado <bgomez-r@student.42madrid.com>
2021-11-13 13:50:28 +0100

    Fixes a bug that caused only the first 3 chars of Builtins to be compared

--- srcs/builtins.c
+++ srcs/builtins.c
@@ -6,7 +6,7 @@
 /*   By: bgomez-r <bgomez-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/26 09:05:10 by bgomez-r          #+#    #+#             */
-/*   Updated: 2021/11/13 13:34:44 by bgomez-r         ###   ########.fr       */
+/*   Updated: 2021/11/13 13:47:49 by bgomez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,13 @@
 
 void	builtins(t_list *env_lst, char *token)
 {
-	if (ft_strncmp(token, "env", 3) == 0)
+	if (ft_strcmp(token, "env") == 0)
 	{
 		ft_lstiter(env_lst, print_env);
 	}
-	else if (ft_strncmp(token, "pwd", 3) == 0)
+	else if (ft_strcmp(token, "pwd") == 0)
 	{
 		builtint_pwd(token);
-//		ft_lstiter(env_lst, print_env);
 	}
 /*
 	else if argumento == "pwd"
