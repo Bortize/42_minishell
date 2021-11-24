17baf5302caac8c534ef1a6b9ecd9bb9482f869f
Borja <bgomez-r@student.42madrid.com>
2021-11-24 21:02:42 +0100

    Fixes an issue that caused environment variables to be misidentified

--- srcs/environment/env_var_cmp.c
+++ srcs/environment/env_var_cmp.c
@@ -6,19 +6,19 @@
 /*   By: bgomez-r <bgomez-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/21 11:53:11 by vicmarti          #+#    #+#             */
-/*   Updated: 2021/11/21 16:27:57 by bgomez-r         ###   ########.fr       */
+/*   Updated: 2021/11/24 20:59:04 by bgomez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 /*
-** This function receives 2 void pointers as parameters and compares their 
-** values. Here we will use it to compare the values of 2 structures of 
+** This function receives 2 void pointers as parameters and compares their
+** values. Here we will use it to compare the values of 2 structures of
 ** type t_env_var where:
 ** @ void *elem1 -> Pointer to structure of type t_env_var (shelevelref)
 ** @ void *elem2 -> Pointer to structure of type t_env_var (lst->content)
-*/ 
+*/
 
 int	env_var_cmp(void *elem1, void *elem2)
 
@@ -26,9 +26,12 @@ int	env_var_cmp(void *elem1, void *elem2)
 	const t_env_var	*ev1 = elem1;
 	const t_env_var	*ev2 = elem2;
 
-	if (ev1 == ev2 || (ev1 && ev2 && ev1->key == ev2->key))
+	if (ft_strcmp(ev1->key, ev2->key) == 0)
+	{
 		return (0);
-	else if (!ev2->key || !ev2->key)
+	}
+	else
+	{
 		return (1);
-	return (ft_strcmp(ev1->key, ev2->key));
+	}
 }
