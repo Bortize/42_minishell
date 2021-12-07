6ffc6b1cdbb936731b83d2856bed19c2f3adc378
Borja Gómez-Rey Delgado <bgomez-r@student.42madrid.com>
2021-12-07 18:10:38 +0100

    Fixes several bugs in the cd builtin, and it looks like there are more to come

--- srcs/builtins/builtins_cd.c
+++ srcs/builtins/builtins_cd.c
@@ -6,31 +6,69 @@
 /*   By: bgomez-r <bgomez-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/20 13:40:52 by bgomez-r          #+#    #+#             */
-/*   Updated: 2021/12/07 00:24:33 by bgomez-r         ###   ########.fr       */
+/*   Updated: 2021/12/07 18:05:39 by bgomez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include <limits.h>
 
+char *set_pwd(t_list *env_lst, char *str)
+{
+	t_env_var shelevelref;
+	t_list	*found_shlvl;
+	t_env_var *tmp;
+
+	shelevelref.key = "PWD";
+	shelevelref.value = NULL;
+	found_shlvl = ft_lst_find(env_lst, &shelevelref, env_var_cmp);
+	tmp = found_shlvl->content;
+	free(tmp->value);
+	tmp->value = ft_strdup(str);
+	return (((t_env_var *)found_shlvl->content)->value);
+}
+
+char *get_oldpwd(t_list *env_lst)
+{
+	t_env_var shelevelref;
+	t_list	*found_shlvl;
+	t_env_var *tmp;
+
+	shelevelref.key = "OLDPWD";
+	shelevelref.value = NULL;
+	found_shlvl = ft_lst_find(env_lst, &shelevelref, env_var_cmp);
+	tmp = found_shlvl->content;
+	return (((t_env_var *)found_shlvl->content)->value);
+}
+
+char	*get_pwd(t_list *env_lst)
+{
+	t_env_var shelevelref;
+	t_list	*found_shlvl;
+	t_env_var *tmp;
+
+	shelevelref.key = "PWD";
+	shelevelref.value = NULL;
+	found_shlvl = ft_lst_find(env_lst, &shelevelref, env_var_cmp);
+	tmp = found_shlvl->content;
+	printf("---get_pwd--- %s=%s\n", tmp->key, tmp->value );
+	return (((t_env_var *)found_shlvl->content)->value);
+}
+
 /*
 ** It searches in 'env_lst' the key OLDPWD to update its value with the
 ** path that is passed by parameter.
 */
 
-char *get_oldpwd(t_list *env_lst, char *str)
+char *set_oldpwd(t_list *env_lst, char *str)
 {
 	t_env_var shelevelref;
 	t_list	*found_shlvl;
 	t_env_var *tmp;
-	char *gor;
-
-	gor = str;
 
 	shelevelref.key = "OLDPWD";
 	shelevelref.value = NULL;
 	found_shlvl = ft_lst_find(env_lst, &shelevelref, env_var_cmp);
-	printf("%p\n", found_shlvl);
 	tmp = found_shlvl->content;
 	free(tmp->value);
 	tmp->value = ft_strdup(str);
@@ -42,13 +80,13 @@ char *get_oldpwd(t_list *env_lst, char *str)
 ** obtain its value and return it as a return.
 */
 
-char	*get_home(t_list *env_lst)
+char	*get_current_path(t_list *env_lst, char *str)
 {
 	t_env_var shelevelref;
 	t_list	*found_shlvl;
 	t_env_var *tmp;
 
-	shelevelref.key = "HOME";
+	shelevelref.key = str;
 	shelevelref.value = NULL;
 	found_shlvl = ft_lst_find(env_lst, &shelevelref, env_var_cmp);
 	tmp = found_shlvl->content;
@@ -66,6 +104,7 @@ void builtins_cd(char **arg, t_list *env_lst)
 
 
 	char *pwd;
+	char *aux;
 	int i;
 
 	i = 0;
@@ -80,23 +119,47 @@ void builtins_cd(char **arg, t_list *env_lst)
 	}
 
 	pwd = getcwd(NULL, 4096);
-	if (ft_strcmp(arg[0], "cd") == 0 || ft_strcmp(arg[1], "--") == 0)
+	if (i == 1)
 	{
-		chdir(get_home(env_lst));
-		free(pwd);
-		return ;
+		set_oldpwd(env_lst, pwd);
+		chdir(get_current_path(env_lst, "HOME"));
+//		free(pwd);
+//		return ;
 	}
-	else if (ft_strcmp(arg[1], "..") == 0)
+	else if (ft_strcmp(arg[0], "cd") == 0 && ft_strcmp(arg[1], "--") == 0)
 	{
-		printf("The old value is %s\n", get_oldpwd(env_lst, pwd));
+		set_oldpwd(env_lst, pwd);
+		chdir(get_current_path(env_lst, "HOME"));
+//		free(pwd);
+//		return ;
+	}
+/*	else if (ft_strcmp(arg[1], "..") == 0)
+	{
+		set_oldpwd(env_lst, pwd);
 		chdir("..");
+		free(pwd);
+		pwd = getcwd(NULL, 4096);
+		set_pwd(env_lst, pwd);
+//		free(pwd);
+//		return ;
+	}*/
+	else if (ft_strcmp(arg[1], "-") == 0)
+	{
+		aux = ft_strdup(get_pwd(env_lst));
+		printf("💥%s\n", aux);
+		chdir(get_oldpwd(env_lst));
+//		set_oldpwd(env_lst, aux);
+//		free(aux)*/
 	}
 	else
 	{
-		printf("La luz %s\n", arg[1]);
-		chdir(arg[1]);
+		if (chdir(arg[1]) == -1)
+			perror("cd");
 	}
+//	printf("💥💥💥💥%s\n", get_pwd(env_lst));
+//	set_pwd(env_lst, get_pwd(env_lst));
 	free(pwd);
+	return ;
 /*
 	else if (ft_strcmp(arg, "-"))
 	{
