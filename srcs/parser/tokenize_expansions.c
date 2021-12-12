a298e0ad3d5e558fcf9e14d24b4988afb1ea069c
Víctor Martínez Estrada <victormartinezestrada@gmail.com>
2021-12-12 21:57:35 +0100

    $ No longer expands within single quotes. Still fails at "'" or '"'

--- srcs/parser/tokenize_expansions.c
+++ srcs/parser/tokenize_expansions.c
@@ -6,30 +6,36 @@
 /*   By: vicmarti <vicmarti@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/11 15:40:52 by vicmarti          #+#    #+#             */
-/*   Updated: 2021/12/12 20:48:29 by vicmarti         ###   ########.fr       */
+/*   Updated: 2021/12/12 21:56:22 by vicmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
-#define SINGLEQ 0b001
-#define DOUBLEQ 0b010
-#define ENV_VAR 0b100
+#define SINGLEQ 0b01
+#define DOUBLEQ 0b10
 
 //Called after reading a non-escaped quote or $.
 static void	update_status(char *status, char ch_read)
 {
-	*status &= ~ENV_VAR;
-	if (~(*status & SINGLEQ) && ch_read == '$')
-		*status |= ENV_VAR;
-	else if ((*status & SINGLEQ) && ch_read == '\'')
-		*status &= ~SINGLEQ;
-	else if ((*status & DOUBLEQ) && ch_read == '\"')
-		*status &= ~DOUBLEQ;
-	else if (ch_read == '\'')
-		*status |= SINGLEQ;
-	else
-		*status |= DOUBLEQ;
+	char	flag;
+
+	flag = *status;
+	if (ch_read == '\'' && !(flag & DOUBLEQ))
+	{
+		if (flag & SINGLEQ)
+			flag &= ~SINGLEQ;
+		else
+			flag |= SINGLEQ;
+	}
+	else if (ch_read == '\"' && !(flag & SINGLEQ))
+	{
+		if (flag & DOUBLEQ)
+			flag &= ~DOUBLEQ;
+		else
+			flag |= DOUBLEQ;
+	}
+	*status = flag;
 }
 
 static char	*get_var_str(t_list *env, char *str)
@@ -51,24 +57,15 @@ static char	*get_var_str(t_list *env, char *str)
 	return (ft_strdup(var_node->value));
 }
 
-static t_list	*add_tk_cln_fail(t_list **tk_lst, char *str,
-					size_t len, t_list  *env)
+static t_list	*add_tk_cln_fail(t_list **tk_lst, char *str)
 {
-	char	*aux_data;
 	t_list	*aux_node;
 
-	if (*str == '$')
-		aux_data = get_var_str(env, str);
-	else
-		aux_data = ft_strndup(str, len);
-			ft_putstr_fd("in it:", 2);
-			ft_putstr_fd(aux_data, 2);
-			ft_putstr_fd("\n", 2);
-	aux_node = ft_lstnew(aux_data);
-	if (!aux_node || !aux_data)
+	aux_node = ft_lstnew(str);
+	if (!aux_node || !str)
 	{
 		perror("Memory unavailable.");
-		free(aux_data);
+		free(str);
 		free(aux_node);
 		ft_lstclear(tk_lst, free);
 		*tk_lst = NULL;
@@ -90,21 +87,21 @@ t_list	*tokenize_expansions(char *line, t_list *env)
 	substr_bgn = line;
 	while (*substr_bgn)
 	{
-		if (*line == '$' && ~(status & SINGLEQ) && line == substr_bgn)
+		update_status(&status, *line);
+		if (*line == '$' && !(status & SINGLEQ) && line == substr_bgn)
 		{
 			line++;
 			line += read_variable(line);
-			if (!add_tk_cln_fail(&tk_lst, substr_bgn, line - substr_bgn, env))
+			if (!add_tk_cln_fail(&tk_lst, get_var_str(env, substr_bgn)))
 				return (NULL);
 			substr_bgn = line;
 		}
-		else if (!*line || (*line == '$' && ~(status & SINGLEQ))
+		else if (!*line || (*line == '$' && !(status & SINGLEQ))
 				|| (~(status & SINGLEQ) && *line == '\"')
 				|| (~(status & DOUBLEQ) && *line == '\''))
 		{
-			if (!add_tk_cln_fail(&tk_lst, substr_bgn, line - substr_bgn, env))
+			if (!add_tk_cln_fail(&tk_lst, ft_strndup(substr_bgn, line - substr_bgn)))
 				return (NULL);
-			update_status(&status, *line);
 			if (*line != '$')
 				line++;
 			substr_bgn = line;
