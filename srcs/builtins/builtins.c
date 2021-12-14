2df1b55df2d1b76c984875281aa8053cf4cc6f1b
Víctor Martínez Estrada <victormartinezestrada@gmail.com>
2021-12-14 22:23:51 +0100

    Builtins return a value, we need to catch it.

--- srcs/builtins/builtins.c
+++ srcs/builtins/builtins.c
@@ -6,7 +6,7 @@
 /*   By: bgomez-r <bgomez-r@student.42madrid.com>>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/26 09:05:10 by bgomez-r          #+#    #+#             */
-/*   Updated: 2021/12/12 22:30:43 by bgomez-r         ###   ########.fr       */
+/*   Updated: 2021/12/14 22:20:28 by vicmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,24 +19,24 @@
 
 int	builtins(t_list **env_lst, char **str_args)
 {
+	g_builtin = 0;
 	if (str_args[0] == NULL)
 		return (-1);
 	g_builtin = 1;
 	if (ft_strcmp(str_args[0], "exit") == 0)
-		builtins_exit(str_args);
-	else if (ft_strcmp(str_args[0], "env") == 0)
-		builtins_env(*env_lst);
-	else if (ft_strcmp(str_args[0], "pwd") == 0)
-		builtint_pwd(str_args[0]);
-	else if (ft_strcmp(str_args[0], "echo") == 0)
-		print_echo_str(str_args);
-	else if (ft_strcmp(str_args[0], "cd") == 0)
-		builtins_cd(str_args, *env_lst);
-	else if (ft_strcmp(str_args[0], "export") == 0)
-		builtins_export(env_lst, str_args);
-	else if (ft_strcmp(str_args[0], "unset") == 0)
-		builtins_unset(env_lst, str_args);
-	else
-		g_builtin = 0;
+		return (builtins_exit(str_args));
+	if (ft_strcmp(str_args[0], "env") == 0)
+		return (builtins_env(*env_lst));
+	if (ft_strcmp(str_args[0], "pwd") == 0)
+		return (builtint_pwd(str_args[0]));
+	if (ft_strcmp(str_args[0], "echo") == 0)
+		return (print_echo_str(str_args));
+	if (ft_strcmp(str_args[0], "cd") == 0)
+		return (builtins_cd(str_args, *env_lst));
+	if (ft_strcmp(str_args[0], "export") == 0)
+		return (builtins_export(env_lst, str_args));
+	if (ft_strcmp(str_args[0], "unset") == 0)
+		return (builtins_unset(env_lst, str_args));
+	g_builtin = 0;
 	return (-1);
 }
